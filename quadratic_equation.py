f33a54f82602a8576255c5fcdf478b5b5a32aec2
Igor Laktionov <ilaktionov@iponweb.net>
2017-07-26 11:38:25 +0300

    refactoring

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,12 +1,12 @@
 from math import sqrt
 
 
-def get_roots(a, b, c):
-    discriminant = b ** 2 - 4 * a * c
+def get_roots(average_coefficient, leading_coefficient, free_member):
+    discriminant = leading_coefficient ** 2 - 4 * average_coefficient * free_member
     if discriminant < 0:
         return None, None
-    root1 = (-b - sqrt(discriminant)) / (2 * a)
-    root2 = (-b + sqrt(discriminant)) / (2 * a)
+    root1 = (-leading_coefficient - sqrt(discriminant)) / (2 * average_coefficient)
+    root2 = (-leading_coefficient + sqrt(discriminant)) / (2 * average_coefficient)
     if discriminant == 0:
         return root1, None
     else:
